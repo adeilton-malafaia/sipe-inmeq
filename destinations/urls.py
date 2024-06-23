64061acdac9760444d2685667e3adc894fa67b30
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2024-06-23 15:22:58 -0300

    Iniciando o uso de Django Tags

--- destinations/urls.py
+++ destinations/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'destinations'
+
 urlpatterns = [
-    path('', views.home),
-    path('entidades/', views.entidades),
-    path('saidas/', views.saidas),
-    path('registrodestino/', views.registrodestino),
-    path('planilha', views.planilha),
+    path('', views.home, name='home'),
+    path('entidades/', views.entidades, name='entidade'),
+    path('saidas/', views.saidas, name='saidas'),
+    path('registrodestino/', views.registrodestino, name='registrodestino'),
+    path('planilha', views.planilha, name='palnilha'),
 ]
