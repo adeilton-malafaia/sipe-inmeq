c44089c705d48bb8f6055fd30f510aa22fda4934
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2024-07-11 12:58:57 -0300

    Atualização forms

--- destinations/forms.py
+++ destinations/forms.py
@@ -5,10 +5,10 @@ from . import models
 # Primeiro item do select de Entidades cadastradas
 op = models.Entidade()
 op.cnpj = 0
-op.rs = 'SELECIONE A ENTIDADE DESEJADA PARA ATUALIZAR DADOS'
+op.rs = "SELECIONE A ENTIDADE DESEJADA PARA ATUALIZAR DADOS"
 
 # Demais itens com todas as entidades cadastradas
-ops = models.Entidade.objects.filter(ativo='s').order_by('rs')
+ops = models.Entidade.objects.filter(ativo="s").order_by("rs")
 
 # Criando o iterável com todos os itens
 options = [(op.cnpj, op.rs)]
@@ -23,49 +23,57 @@ class EntidadeForm(forms.ModelForm):
     class Meta:
         model = models.Entidade
         fields = [  # type: ignore
-            'cnpj',
-            'rs',
-            'nf',
-            'contatos',
-            'email',
-            'fones',
-            'validade',
-            'ativo',
+            "cnpj",
+            "rs",
+            "nf",
+            "contatos",
+            "email",
+            "fones",
+            "validade",
+            "ativo",
         ]
 
         labels = {
-            'rs': 'Razão Social',
-            'nf': 'Nome de Fantasia',
-            'email': 'E-mail',
-            'fones': 'Telefones',
-            'validade': 'Validade do cadastro',
-            'ativo': 'Ativo?'
+            "rs": "Razão Social",
+            "nf": "Nome de Fantasia",
+            "email": "E-mail",
+            "fones": "Telefones",
+            "validade": "Validade do cadastro",
+            "ativo": "Ativo?",
         }
 
         help_texts = {
-            'rs': 'A razão social é obrigatória',
-            'ativo': 'Você deve selecionar uma opção'
+            "rs": "A razão social é obrigatória",
+            "ativo": "Você deve selecionar uma opção",
+            "validade": "A data de validade é obrigatória",
         }
 
         error_messages = {
-            'cnpj': {
-                'required': 'Este campo não pode ficar vazio',
-                'invalid': 'Digite apenas números'
-            }
+            "cnpj": {
+                "required": "Este campo não pode ficar vazio",
+                "invalid": "Digite apenas números",
+            },
+            "rs": {"required": "A razão social não pode ficar vazia"},
+            "validade": {"required": "Defina uma data para a validade"},
+            "contatos": {"required": "Defina pelo menos um contato"},
         }
 
         widgets = {
-            'cnpj': forms.TextInput(),
-            'ativo': forms.RadioSelect(
-                choices=[('s', 'Sim'), ('n', 'Não')],
+            "cnpj": forms.TextInput(
                 attrs={
-                    'class': 'div-row-left',
+                    "placeholder": "Digite apenas números",
                 }
             ),
-            'validade': forms.DateInput(attrs={'type': 'date'})
+            "ativo": forms.RadioSelect(
+                choices=[("s", "Sim"), ("n", "Não")],
+                attrs={
+                    "class": "div-row-left",
+                },
+            ),
+            "validade": forms.DateInput(attrs={"type": "date"}),
         }
 
     select_entidade = forms.ChoiceField(
         choices=(options),
-        label='Entidades cadastradas:',
+        label="Entidades cadastradas:",
     )
--- destinations/views.py
+++ destinations/views.py
@@ -6,40 +6,46 @@ from . import forms, models
 
 
 def home(request):
-    return render(request, 'destinations/pages/index.html')
+    return render(request, "destinations/pages/index.html")
 
 
 def entidades(request):  # View de rota para cadastro de entidades
-    entidades = models.Entidade.objects.all().order_by('rs').filter(ativo='s')
-    return render(request,
-                  'destinations/pages/cadastro-entidade.html',
-                  context={'entidades': entidades})
+    entidades = models.Entidade.objects.all().order_by("rs").filter(ativo="s")
+    return render(
+        request,
+        "destinations/pages/cadastro-entidade.html",
+        context={"entidades": entidades},
+    )
 
 
 def cadentidades(request):  # View de rota para cadastro de entidades
     if request.POST:
         POST = request.POST
-        request.session['register_form_entidades'] = POST
+        request.session["register_form_entidades"] = POST
         form = forms.EntidadeForm(POST)
     else:
         form = forms.EntidadeForm()
-    return render(request,
-                  'destinations/pages/entidades.html',
-                  context={'form': form, }
-                  )
+
+    return render(
+        request,
+        "destinations/pages/entidades.html",
+        context={
+            "form": form,
+        },
+    )
 
 
 def saidas(request):  # View de rota para registro de destino de produtos
-    return render(request,
-                  'destinations/pages/registro-saidas.html',
-                  context={
-                      'saidas': [makeSaida() for _ in range(30)]
-                  })
+    return render(
+        request,
+        "destinations/pages/registro-saidas.html",
+        context={"saidas": [makeSaida() for _ in range(30)]},
+    )
 
 
 def registrodestino(request):  # View de rota para registro de doação
-    return render(request, 'destinations/pages/registro-destino.html')
+    return render(request, "destinations/pages/registro-destino.html")
 
 
 def planilha(request):  # View de rota para planilha de produtos p/ doação
-    return render(request, 'destinations/pages/gerar-planilha.html')
+    return render(request, "destinations/pages/gerar-planilha.html")
