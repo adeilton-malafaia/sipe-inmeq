61e6983de5b4fc2b2a8ad28db1ca0ef9f47663a9
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2024-07-24 17:23:44 -0300

    Testes unitários para urls

--- destinations/tests.py
+++ destinations/tests.py
@@ -1,6 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class DestinationsURLsTests(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    def test_url_destinations_home_is_correct(self):
+        url = reverse('destinations:entidade-insert')
+        self.assertEqual(url, '/entidades/cadastro/')
+
+    def test_url_destinations_entidades_cadastro_is_correct(self):
+        url = reverse('destinations:entidade-insert')
+        self.assertEqual(url, '/entidades/cadastro/')
+
+    def test_url_destinations_entidades_update_is_correct(self):
+        url = reverse('destinations:entidade-update')
+        self.assertEqual(url, '/entidades/atualizar/')
+
+    def test_url_destinations_entidades_registrodestino_is_correct(self):
+        url = reverse('destinations:entidade-registrodestino')
+        self.assertEqual(url, '/entidades/registrodestino/')
--- destinations/urls.py
+++ destinations/urls.py
@@ -6,10 +6,10 @@ app_name = 'destinations'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('entidades/cadastro', views.entidades_insert, name='entidade-insert'),
-    path('entidades/atualizar', views.entidades_update,
+    path('entidades/cadastro/', views.entidades_insert, name='entidade-insert'),  # noqa: E501
+    path('entidades/atualizar/', views.entidades_update,
          name='entidade-update'),  # ignore
     path('saidas/', views.saidas, name='saidas'),
-    path('registrodestino/', views.registrodestino, name='registrodestino'),
-    path('planilha', views.planilha, name='palnilha'),
+    path('entidades/registrodestino/', views.registrodestino, name='entidade-registrodestino'),  # noqa: E501
+    path('planilha/', views.planilha, name='entidades-planilha'),
 ]
